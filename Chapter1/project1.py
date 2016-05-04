07f13e396af5db1186bee392e3d2ccfb8e0d9193
Graham Hough <graham.hough117@gmail.com>
2016-05-03 20:13:58 -0400

    fist commmit and chapter 2

---
+++ Chapter1/project1.py
@@ -0,0 +1,5 @@
+messages = "Hello Python world!"
+print(message)
+
+messages = "Hello Python Crash Course World!"
+print(message)
---
+++ Chapter1/project2.py
@@ -0,0 +1,21 @@
+name = "ada lovelace"
+print(name.title())
+
+
+name = "ada lovelace"
+print(name.upper())
+print(name.lower())
+
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + last_name
+
+print(full_name)
+print("Hello, " + full_name.title() + "!")
+
+favorite_language = '   python '
+print(favorite_language.rstrip())
+print(favorite_language.lstrip())
+print(favorite_language.strip())
+
